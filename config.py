e106c4cd1937326de5e33b4564927d1734b60d91
romiw00rk-stack <romiw00rk@gmail.com>
2026-08-06 22:31:50 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+TOKEN = '8483915034:AAGBY8ssHFQCWLzkvoa7dCupw0rSiqbgeq4'
+ADMIN_ID = 580588329 
+CARD_NUMBER = "5022291095892657" 
+CARD_NAME = "رومینا" 
+SUPPORT_USERNAME = "@mafiasho_admin"
+BAD_WORDS = ["کلمه۱", "کلمه۲", "کلمه۳"] # کلمات ممنوعه را اینجا کامل کن
+
+ROOM_LINKS = [
+    "https://t.me/+HJ1A76sBGEAwYTdk", "https://t.me/+DWnJJcyBkmhhZTBk",
+    "https://t.me/+X-r4xtXJaXpkMzJk", "https://t.me/+p1Estt71cU45NzBk",
+    "https://t.me/+RLbTxBn7Bd9jOWM0", "https://t.me/+4xuDGo_zD7IxNDU0",
+    "https://t.me/+P0yblouGcJphNjI0", "https://t.me/+e01UDD1ALU42ZjQ8",
+    "https://t.me/+h1tTM4wvK0M1ZjU0", "https://t.me/+yBIzkezu7HA0MTA8"
+]
